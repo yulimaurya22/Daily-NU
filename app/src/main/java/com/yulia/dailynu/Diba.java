29c277bb5cebfbf90962a6bdbd3b0fa76a17294f
yulimaurya22 <yulimaurya@gmail.com>
2023-01-08 12:45:06 +0700

    menambahkan icon Back pada actionbar

--- app/src/main/java/com/yulia/dailynu/Diba.java
+++ app/src/main/java/com/yulia/dailynu/Diba.java
@@ -1,14 +1,31 @@
 package com.yulia.dailynu;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
 public class Diba extends AppCompatActivity {
 
+    ActionBar actionBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diba);
+        getSupportActionBar().setTitle("DIBA'");
+        actionBar = getSupportActionBar();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp(){
+        onBackPressed();
+        return true;
+    }
+
+    @Override
+    public void onBackPressed(){
+        super.onBackPressed();
     }
 }
